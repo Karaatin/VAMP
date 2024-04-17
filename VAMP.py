61ecaa1fd3b57e5ee30476066a5090181b2bca38
Karaatin <Keanu2712@gmail.com>
2024-04-17 23:52:04 +0200

    Clarified feedback of VAMP

--- VAMP.py
+++ VAMP.py
@@ -327,7 +327,7 @@ class Analyzer:
             print("The content of the " + str(s + 1) + ". section can't be analysed, because the type field " + str(
                 tf) + " is unknown till now.")
             self.results += (
-                    "The content of the " + str(s + 1) + ". can't be analysed, because the type field is unknown till now.\n\n")
+                    "The content of the " + str(s + 1) + ". can't be analysed, because the method to decrypt this type field is unknown till now.\n\n")
             return
         else:
 
